f2d9e2522a542e53e95f7f49cd4d43185488ef59
Albert Li <albert.li@gmail.com>
2018-07-08 15:27:24 +0800

    Supports Thai and Indonesian

---
+++ GPXTracker/widget/GPXTrackerWidget/locales/id-id/language.js
@@ -0,0 +1,12 @@
+/* Localized strings contents for Indonesian [Indonesia] */ 
+
+TIZEN_L10N=
+{
+	"do_not_use" : "id",
+	"deviation" : "Deviasi",
+	"distance_remaining" : "Jarak Sisa",
+	"no_route_info" : "Tidak Ada Informasi Route",
+	"last_updated" : "Terakhir Diperbarui",
+	"set_route" : "Set Rute Di Gpx Tracker",
+	"no_data" : "Tidak Ada Data. Aktifkan Ikuti Route"
+};
---
+++ GPXTracker/widget/GPXTrackerWidget/locales/th-th/language.js
@@ -0,0 +1,12 @@
+/* Localized strings contents for Thai [Thailand] */ 
+
+TIZEN_L10N=
+{
+	"do_not_use" : "th",
+	"deviation" : "การเบี่ยงเบน",
+	"distance_remaining" : "ระยะทางที่เหลือ",
+	"no_route_info" : "ไม่มีข้อมูลเส้นทาง",
+	"last_updated" : "อัพเดทล่าสุด",
+	"set_route" : "ตั้งในเส้นทาง Gpx ติดตาม",
+	"no_data" : "ไม่มีข้อมูล. เปิดใช้งานการติดตามเส้นทาง"
+};
